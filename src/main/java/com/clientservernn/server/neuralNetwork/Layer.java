16275fe2ef2642345f748fe203d6a3307cd954f9
JeniksSL <iba.slabko1.22@gmail.com>
2023-01-27 17:58:40 +0300

    Network

---
+++ src/main/java/com/clientservernn/server/neuralNetwork/Layer.java
@@ -0,0 +1,75 @@
+package com.clientservernn.server.neuralNetwork;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.function.DoubleUnaryOperator;
+
+
+/**
+ * The class {@code Layer} represents one layer of neural network.
+ * Implementation borrowed from David Kopec:
+ * <a href="https://github.com/davecom/ClassicComputerScienceProblemsInJava">
+ *     https://github.com/davecom/ClassicComputerScienceProblemsInJava</a>
+ *
+ * @author David Kopec
+ * @since 1.0
+ */
+public class Layer {
+    public Optional<Layer> previousLayer;
+    public List<Neuron> neurons = new ArrayList<>();
+    public double[] outputCache;
+
+    public Layer(Optional<Layer> previousLayer, int numNeurons, double learningRate, DoubleUnaryOperator activationFunction, DoubleUnaryOperator derivativeActivationFunction) {
+        this.previousLayer = previousLayer;
+        Random random = new Random();
+        for(int i = 0; i < numNeurons; ++i) {
+            double[] randomWeights = null;
+            if (previousLayer.isPresent()) {
+                randomWeights = random.doubles((previousLayer.get()).neurons.size()).toArray();
+            }
+            Neuron neuron = new Neuron(randomWeights, learningRate, activationFunction, derivativeActivationFunction);
+            this.neurons.add(neuron);
+        }
+
+        this.outputCache = new double[numNeurons];
+    }
+
+    public double[] outputs(double[] inputs) {
+        if (this.previousLayer.isPresent()) {
+            this.outputCache = this.neurons.stream().mapToDouble((n) -> n.output(inputs)).toArray();
+        } else {
+            this.outputCache = inputs;
+        }
+        return this.outputCache;
+    }
+
+    public void calculateDeltasForOutputLayer(double[] expected) {
+        for(int n = 0; n < this.neurons.size(); ++n) {
+            (this.neurons.get(n)).delta = (this.neurons.get(n)).derivativeActivationFunction.applyAsDouble((this.neurons.get(n)).outputCache) * (expected[n] - this.outputCache[n]);
+        }
+    }
+
+    public void calculateDeltasForHiddenLayer(Layer nextLayer) {
+        for (int i = 0; i < neurons.size(); i++) {
+            int index = i;
+            double[] nextWeights = nextLayer.neurons.stream().mapToDouble(n -> n.weights[index]).toArray();
+            double[] nextDeltas = nextLayer.neurons.stream().mapToDouble(n -> n.delta).toArray();
+            double sumWeightsAndDeltas = Util.dotProduct(nextWeights, nextDeltas);
+            neurons.get(i).delta = neurons.get(i).derivativeActivationFunction
+                    .applyAsDouble(neurons.get(i).outputCache) * sumWeightsAndDeltas;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        String layerDescription = "Layer, neurons: \n";
+        for (int i = 0; i < neurons.size(); i++) {
+            layerDescription=layerDescription.concat(neurons.get(i).toString()).concat("\n");
+        }
+        return layerDescription;
+    }
+}
---
+++ src/main/java/com/clientservernn/server/neuralNetwork/Network.java
@@ -0,0 +1,99 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.clientservernn.server.neuralNetwork;
+
+import com.clientservernn.server.utilities.RawCharData;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.DoubleUnaryOperator;
+import java.util.function.Function;
+
+
+
+/**
+ * The class {@code Network} represents neural network which operate
+ * any type object.
+ * Implementation borrowed from David Kopec:
+ * <a href="https://github.com/davecom/ClassicComputerScienceProblemsInJava">
+ *     https://github.com/davecom/ClassicComputerScienceProblemsInJava</a>
+ * @param <T> the type of objects operated by the {@code Network}
+ *
+ * @author David Kopec
+ * @author Yauheni Slabko
+ * @since 1.0
+ */
+public class Network<T> {
+    private final List<Layer> layers = new ArrayList<>();
+    public Network(int[] layerStructure, double learningRate, DoubleUnaryOperator activationFunction, DoubleUnaryOperator derivativeActivationFunction) {
+        Layer inputLayer = new Layer(Optional.empty(), layerStructure[0], learningRate, activationFunction, derivativeActivationFunction);
+        this.layers.add(inputLayer);
+        for(int i = 1; i < layerStructure.length; ++i) {
+            Layer nextLayer = new Layer(Optional.of(this.layers.get(i - 1)), layerStructure[i], learningRate, activationFunction, derivativeActivationFunction);
+            this.layers.add(nextLayer);
+        }
+
+    }
+
+    private double[] getOutputs(double[] input) {
+        double[] result = input;
+        Layer layer;
+        for (Layer value : this.layers) {
+            layer = value;
+            result = layer.outputs(result);
+        }
+        return result;
+    }
+
+    private void backpropagate(double[] expected) {
+        int lastLayer = this.layers.size() - 1;
+        (this.layers.get(lastLayer)).calculateDeltasForOutputLayer(expected);
+
+        for(int i = lastLayer - 1; i >= 0; --i) {
+            (this.layers.get(i)).calculateDeltasForHiddenLayer(this.layers.get(i + 1));
+        }
+
+    }
+
+    private void updateWeights() {
+        for (Layer layer : this.layers.subList(1, this.layers.size())) {
+            for (Neuron neuron : layer.neurons) {
+                for (int w = 0; w < neuron.weights.length; ++w) {
+                    neuron.weights[w] += neuron.learningRate * ((Layer) layer.previousLayer.get()).outputCache[w] * neuron.delta;
+                }
+            }
+        }
+
+    }
+
+    public void train(List<double[]> inputs, List<double[]> expects) {
+        for(int i = 0; i < inputs.size(); ++i) {
+            double[] xs = (double[])inputs.get(i);
+            double[] ys = (double[])expects.get(i);
+            this.getOutputs(xs);
+            this.backpropagate(ys);
+            this.updateWeights();
+        }
+
+    }
+
+    public HashMap<T, Double> getCheck(byte[] imageData, Function<double[], HashMap<T, Double>> interpret) {
+        double[] result = this.getOutputs(RawCharData.getDoublesArrayRefactored(imageData));
+        return interpret.apply(result);
+    }
+
+    @Override
+    public String toString() {
+        String layersDescription="Neural network, layers: \n";
+        for (int i = 0; i < layers.size(); i++) {
+            layersDescription= layersDescription.concat(layers.get(i).toString()).concat("\n");
+        }
+        return layersDescription;
+
+    }
+}
